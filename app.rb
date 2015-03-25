8328eca8ca290338d1ebea41e6ac8134c80d14ea
Marc Picaud <picaud_m@etna-alternance.net>
2015-03-25 11:40:02 +0100

    DB connection

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require 'sinatra'
+require 'active_record'
+require 'mysql2'
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => "mysql2",
+  :host     => "localhost",
+  :username => "root",
+  :password => "root",
+  :database => "rest"
+)
+
+class User < ActiveRecord::Base
+end
+
 set :bind, '0.0.0.0'
 
 get '/' do
