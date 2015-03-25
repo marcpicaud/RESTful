c076eb15e0d6ed9cd8e763f10db591d613da8b72
Marc Picaud <picaud.marc@gmail.com>
2015-03-26 00:06:38 +0100

    pre-commit

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ ActiveRecord::Base.establish_connection(
   :adapter  => "mysql2",
   :host     => "localhost",
   :username => "root",
-  :password => "root",
+  :password => "sagemmy-x5",
   :database => "rest"
 )
 
@@ -22,4 +22,45 @@ end
 get '/search/users' do
   mail = params[:q]
   User.where(email = mail).to_json
+end
+
+delete '/user/:id' do
+  content_type :json
+  User.find_by_id(params[:id]).destroy
+end
+
+post '/users' do
+  content_type :json
+  {"params" => params}.to_json
+  begin
+    user = User.create(
+      :lastname => params[:lastname],
+      :firstname => params[:firstname],
+      :email => params[:email],
+      :password => params[:password],
+      :role => params[:role]
+    )
+  rescue
+    "ERROR!".to_json
+  end
+  #user.to_json
+end
+
+put '/users/:id' do
+  #content_type :json
+  my_user = User.find_by_id(params[:id])
+  wanted = %w[lastname firstname email password role]
+  puts params.inspect
+  #params.each do |k, v|
+    #if k.class == String and wanted.include? k
+     # begin
+      #  k.inspect
+       # v.inspect
+        #my_user[k] = v
+      #rescue
+      #end
+    #end
+    #puts k.inspect
+    #puts v.inspect
+  #end
 end
